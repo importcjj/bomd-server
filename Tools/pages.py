b940252db05db77b782bf1e6fd3709339484639a
Importcjj <importcjj@gmail.com>
2015-07-19 16:16:39 +0800

    墨绿色主题

--- Tools/pages.py
+++ Tools/pages.py
@@ -11,21 +11,16 @@ def pageHandle(all_items, items_num, pageID):
         pageID = 1
     
     length = len(all_items)
-    maxPage = length / items_num
-    remainder = length % items_num
+    (maxPage, remainder)= divmod(length, items_num)
     if remainder > 0 : maxPage += 1
     
-    if pageID < 1:
-        pageID = 1
-    elif pageID > maxPage:
-        PageID = maxPage
+    if pageID < 1 : pageID = 1
+    if pageID > maxPage : PageID = maxPage
 
+    start_index = items_num*(pageID-1)
     if pageID == maxPage and remainder:
-        items = all_items[items_num * (pageID - 1) : items_num * (pageID - 1) + remainder + 1]
+        items = all_items[start_index : start_index + remainder + 1]
     else:
-        items = all_items[items_num * (pageID - 1) : items_num * (pageID - 1) + items_num]
-        
-        
-    
-    
-    return [maxPage, pageID, items]
+        items = all_items[start_index : start_index + items_num]
+
+    return (maxPage, pageID, items)
